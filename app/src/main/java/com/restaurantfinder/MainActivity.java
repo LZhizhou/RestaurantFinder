aaa2fd7d006ac591faf131005eb3563e17f1e98f
Peiran Yu <46502584+TaraYu@users.noreply.github.com>
2020-01-18 15:31:25 -0700

    adapter

--- app/src/main/java/com/restaurantfinder/MainActivity.java
+++ app/src/main/java/com/restaurantfinder/MainActivity.java
@@ -2,10 +2,24 @@ package com.restaurantfinder;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private List<String> myDatas = new ArrayList<String>();
+    private MyAdapter myAdapter;
+    private Button addButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +27,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 //        Intent intent = new Intent(MainActivity.this, MapsActivity.class);
 //        startActivity(intent);
+        initView();
+        initRecycle();
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myAdapter.addItem(myDatas.size());
+            }
+        });
+    }
+
+    protected ArrayList<String> initData(){
+        ArrayList<String> myDatas = new ArrayList<String>();
+        for(int i=0;i<1;i++){
+            myDatas.add("this is person"+ (i+1));
+        }
+        return myDatas;
+
+    }
+
+    private void initView(){
+        addButton = (Button) findViewById(R.id.my_recycler_view);
+        recyclerView = (RecyclerView)findViewById(R.id.my_recycler_view);
+
+    }
+
+    private void initRecycle(){
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        myDatas = initData();
+        myAdapter = new MyAdapter(MainActivity.this,myDatas);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
     }
 }
