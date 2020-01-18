e741e76beebaa288fc766361a924818f654993e2
Zhizhou Lu <luzhizhou@hotmail.com>
2020-01-18 12:36:36 -0700

    start

---
+++ app/src/main/java/com/restaurantfinder/MainActivity.java
@@ -0,0 +1,17 @@
+package com.restaurantfinder;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+//        startActivity(intent);
+    }
+}
